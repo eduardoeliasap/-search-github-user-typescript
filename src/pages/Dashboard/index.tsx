45fbaf17e61935cff788c2f51e8095f74c91d85f
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-11-05 15:11:03 -0300

    Dashboard stlyles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no GitHub</Title>
+  return (
+    <>  
+      <img src={logo} alt="GitHub Repository" />
+      <Title>Explore repositorios no GitHub</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img src="#" alt="Eduardo Elias"
+          
+          />
+          <div>
+            <strong>Eduardo Elias</strong>
+            <p>Repository description </p>
+          </div>       
+
+          <FiChevronRight size={20} />   
+        </a>
+
+        <a href="test">
+          <img src="#" alt="Eduardo Elias"
+          
+          />
+          <div>
+            <strong>Eduardo Elias</strong>
+            <p>Repository description </p>
+          </div>       
+
+          <FiChevronRight size={20} />   
+        </a>
+
+        <a href="test">
+          <img src="#" alt="Eduardo Elias"
+          
+          />
+          <div>
+            <strong>Eduardo Elias</strong>
+            <p>Repository description </p>
+          </div>       
+
+          <FiChevronRight size={20} />   
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
